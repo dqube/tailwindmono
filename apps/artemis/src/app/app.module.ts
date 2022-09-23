26da274debbcbe88f23ffce71baf3c0e662f9dc9
dqube <mdevendran@outlook.com>
2022-09-23 19:12:09 +0530

    svg icon

--- apps/artemis/src/app/app.module.ts
+++ apps/artemis/src/app/app.module.ts
@@ -1,15 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+import { SvgIconModule } from '@app/svg-icon';
+import { AngularSvgIconPreloaderModule } from '@app/svg-icon/pre-loader';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    SvgIconModule.forRoot(),
+    AngularSvgIconPreloaderModule.forRoot({
+      configUrl: './assets/json/icons.json',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
